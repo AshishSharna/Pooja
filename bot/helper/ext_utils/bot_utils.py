8f21fec4f03fc14c550f262eb2adb00019102afc
Ashish Sharma <99271761+AshishSharma2006@users.noreply.github.com>
2022-08-09 10:31:50 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -246,7 +246,7 @@ T: {disk_t}GB | F: {disk_f}GB
 Working For: {currentTime}
 T-DL: {recv} | T-UL: {sent}
 
-Made with ❤️ by Ashish Sharma (@AshishSharmaLegend)
+Made with ❤️ by Ashish Sharma
 """
 
 #---Thanks for deleting my name ❤️ Appreciate it---#
