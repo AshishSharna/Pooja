14e69456cf729c9076332b5848c74d2796506053
Ashish Sharma <99271761+AshishSharma2006@users.noreply.github.com>
2022-08-09 16:28:01 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -120,13 +120,13 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     p_str = '❤' * cFull
-    p_str += '♡' * (12 - cFull)
+    p_str += '🖤' * (12 - cFull)
     p_str = f"{p_str}"
     return p_str
 
 def progress_bar(percentage):
-    p_used = '🖤'
-    p_total = '♡'
+    p_used = '❤'
+    p_total = '🖤'
     if isinstance(percentage, str):
         return 'NaN'
     try:
